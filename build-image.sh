2109ce0d6bf9bd2ab0a9fa7c7e7cf2b56410819e
Sjors Gielen <sjors@sjorsgielen.nl>
2019-11-09 14:05:57 +0100

    Bump to k3os 0.6.0.

--- build-image.sh
+++ build-image.sh
@@ -85,7 +85,7 @@ elif [ "$IMAGE_TYPE" = "orangepipc2" ]; then
 	fi
 fi
 
-dl_dep k3os-rootfs-arm64.tar.gz https://github.com/rancher/k3os/releases/download/v0.5.0/k3os-rootfs-arm64.tar.gz
+dl_dep k3os-rootfs-arm64.tar.gz https://github.com/rancher/k3os/releases/download/v0.6.0/k3os-rootfs-arm64.tar.gz
 
 # To find the URL for these packages:
 # - Go to https://launchpad.net/ubuntu/bionic/arm64/<package name>/
