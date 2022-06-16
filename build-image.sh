2334877fc949c8e840abf5800e77b2e19401d916
CaptainStandby <18215579+CaptainStandby@users.noreply.github.com>
2022-06-16 12:05:49 +0200

    fix bad path and removed `-a` flag from `tee`

--- build-image.sh
+++ build-image.sh
@@ -263,7 +263,7 @@ sudo tar -xf deps/k3os-rootfs-arm64.tar.gz --strip 1 -C root
 sudo cp -R config root/k3os/system
 
 find "config/" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
-	envsubst < "$file" | sudo tee -a "root/k3os/system/$(basename "$file")" > /dev/null
+	envsubst < "$file" | sudo tee "root/k3os/system/config/$(basename "$file")" > /dev/null
 done
 
 for filename in root/k3os/system/config/*.*; do [ "$filename" != "${filename,,}" ] && sudo mv "$filename" "${filename,,}" ; done 
